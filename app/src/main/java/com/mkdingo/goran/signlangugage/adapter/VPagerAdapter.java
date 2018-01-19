a6e9a49559893017aee585d9b6f11d9387a66446
Goran <gogovoinovski@gmail.com>
2018-01-19 14:21:27 +0100

    3.2

--- app/src/main/java/com/mkdingo/goran/signlangugage/adapter/VPagerAdapter.java
+++ app/src/main/java/com/mkdingo/goran/signlangugage/adapter/VPagerAdapter.java
@@ -4,11 +4,19 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.mkdingo.goran.signlangugage.klasi.Zborovi;
+
+import java.util.ArrayList;
+
 /**
  * Created by Goran on 1/19/2018.
  */
 
 public class VPagerAdapter extends FragmentPagerAdapter{
+
+    ArrayList<Zborovi> zborovi;
+
+    public void addSliki(ArrayList<Zborovi> zbor){zborovi = zbor;}
 
 
     public VPagerAdapter(FragmentManager fm) {
