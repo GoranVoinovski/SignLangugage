f30f73265679ce4d5f67b055d22da64813c55053
Goran <gogovoinovski@gmail.com>
2018-01-19 14:35:17 +0100

    3.3

--- app/src/main/java/com/mkdingo/goran/signlangugage/adapter/VPagerAdapter.java
+++ app/src/main/java/com/mkdingo/goran/signlangugage/adapter/VPagerAdapter.java
@@ -1,9 +1,11 @@
 package com.mkdingo.goran.signlangugage.adapter;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.mkdingo.goran.signlangugage.fragment.FragmentSlika;
 import com.mkdingo.goran.signlangugage.klasi.Zborovi;
 
 import java.util.ArrayList;
@@ -25,11 +27,18 @@ public class VPagerAdapter extends FragmentPagerAdapter{
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        Bundle args = new Bundle();
+        args.putString("zbor", zborovi.get(position).getText());
+        args.putSerializable("sliki", zborovi.get(position).getContents());
+        args.putSerializable("bukci", zborovi.get(position).getBukvi());
+        args.putInt("pozicija",position);
+        FragmentSlika fragment = new FragmentSlika();
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return zborovi.size();
     }
 }
--- app/src/main/java/com/mkdingo/goran/signlangugage/klasi/Zborovi.java
+++ app/src/main/java/com/mkdingo/goran/signlangugage/klasi/Zborovi.java
@@ -7,7 +7,31 @@ import java.util.ArrayList;
  */
 
 public class Zborovi {
-    ArrayList<Content> contents;
-    String text;
-    ArrayList<Simboli> bukvi;
+  public ArrayList<Content> contents;
+  public String text;
+  public ArrayList<Simboli> bukvi;
+
+    public ArrayList<Content> getContents() {
+        return contents;
+    }
+
+    public void setContents(ArrayList<Content> contents) {
+        this.contents = contents;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public ArrayList<Simboli> getBukvi() {
+        return bukvi;
+    }
+
+    public void setBukvi(ArrayList<Simboli> bukvi) {
+        this.bukvi = bukvi;
+    }
 }
