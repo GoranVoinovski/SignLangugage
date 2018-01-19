9e5b6fe8bcb6d64451c915a0cde2705a685e31cd
Goran <gogovoinovski@gmail.com>
2018-01-19 12:59:57 +0100

    Pocetno

---
+++ app/src/main/java/com/mkdingo/goran/signlangugage/Najava.java
@@ -0,0 +1,13 @@
+package com.mkdingo.goran.signlangugage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Najava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_najava);
+    }
+}
