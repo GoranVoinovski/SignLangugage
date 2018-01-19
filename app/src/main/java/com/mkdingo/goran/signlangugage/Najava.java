4c768f39cfd3963d4d65d1fac5c0dea28964d647
Goran <gogovoinovski@gmail.com>
2018-01-19 13:18:25 +0100

    1.2

--- app/src/main/java/com/mkdingo/goran/signlangugage/Najava.java
+++ app/src/main/java/com/mkdingo/goran/signlangugage/Najava.java
@@ -2,8 +2,12 @@ package com.mkdingo.goran.signlangugage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
 
 public class Najava extends AppCompatActivity {
+    @BindView(R.id.edtname)EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
