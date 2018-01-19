ba91f75b220875d232aca0f4cbcae6c460fbe1c8
oliverMKD <oliverdamjan2013@gmail.com>
2018-01-19 14:27:05 +0100

    1.7

--- app/src/main/java/com/mkdingo/goran/signlangugage/sharedPreferences/SharedPreferences.java
+++ app/src/main/java/com/mkdingo/goran/signlangugage/sharedPreferences/SharedPreferences.java
@@ -3,13 +3,27 @@ package com.mkdingo.goran.signlangugage.sharedPreferences;
 import android.app.Activity;
 import android.content.Context;
 
+import com.google.gson.Gson;
+import com.mkdingo.goran.signlangugage.klasi.User;
+
 /**
  * Created by Oliver on 1/19/2018.
  */
 
 public class SharedPreferences {
+
+    private static final String addUser = "name";
     private static android.content.SharedPreferences getPreferences(Context context) {
         return context.getApplicationContext().getSharedPreferences("MySharedPreffsFile", Activity.MODE_PRIVATE);
     }
-    String addUser;
+
+    public static void addUser (User user, Context context){
+        Gson gson = new Gson();
+        String mapString = gson.toJson(user);
+        getPreferences(context).edit().putString("USER",mapString).apply();
+    }
+
+    public static String getAddUser() {
+        return addUser;
+    }
 }
