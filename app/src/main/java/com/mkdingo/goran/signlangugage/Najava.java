1569476502b9e43a55815a446fefa8a45d15f266
Goran <gogovoinovski@gmail.com>
2018-01-21 13:39:51 +0100

    1.92,2.6,3.7

--- app/src/main/java/com/mkdingo/goran/signlangugage/Najava.java
+++ app/src/main/java/com/mkdingo/goran/signlangugage/Najava.java
@@ -22,6 +22,7 @@ public class Najava extends AppCompatActivity {
     @BindView(R.id.edtname)EditText userName;
     @BindView(R.id.save)Button saveUser;
     @BindView(R.id.textView)TextView textView;
+    User user;
 
 
     @Override
@@ -29,7 +30,7 @@ public class Najava extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_najava);
         ButterKnife.bind(this);
-
+        user = FillClasses();
 
 
     }
@@ -37,27 +38,50 @@ public class Najava extends AppCompatActivity {
 
     @OnClick(R.id.save)
     public void SaveUser(){
+
+        user.name = userName.getText().toString();
+        SharedPreferences.addUser(user,Najava.this);
+        Intent intent = new Intent(Najava.this, Home.class);
+        startActivity(intent);
+
+    }
+
+    public User FillClasses(){
+
+        User user = new User();
+        user.zborovi = new ArrayList<>();
+
+
         Zborovi zbor1 = new Zborovi();
-        Zborovi zbor2 = new Zborovi();
-        Zborovi zbor3 = new Zborovi();
         zbor1.contents = new ArrayList<>();
         zbor1.bukvi = new ArrayList<>();
         zbor1.text = "Hello";
         Content content1 = new Content();
         content1.Slika = R.raw.yellow;
         zbor1.contents.add(content1);
-        zbor2 = zbor1;
-        zbor3 = zbor2;
 
-        User user = new User();
-        user.zborovi = new ArrayList<>();
+        Zborovi zbor2 = new Zborovi();
+        zbor2.contents = new ArrayList<>();
+        zbor2.bukvi = new ArrayList<>();
+        zbor2.text = "Thank you";
+        Content content2 = new Content();
+        content2.Slika = R.raw.thankyoublue;
+        zbor1.contents.add(content2);
+
+        Zborovi zbor3 = new Zborovi();
+        zbor3.contents = new ArrayList<>();
+        zbor3.bukvi = new ArrayList<>();
+        zbor3.text = "Turn left";
+        Content content3 = new Content();
+        content3.Slika = R.raw.turnleftred;
+        zbor1.contents.add(content3);
+
         user.zborovi.add(zbor1);
         user.zborovi.add(zbor2);
         user.zborovi.add(zbor3);
-        user.name = userName.getText().toString();
-        SharedPreferences.addUser(user,Najava.this);
-        Intent intent = new Intent(Najava.this, Home.class);
-        startActivity(intent);
 
+
+
+        return user;
     }
 }
